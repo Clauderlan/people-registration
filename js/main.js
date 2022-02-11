15e05e0ea2487f689e7724b9511678bf46ccf768
Clauderlan <89857887+Clauderlan@users.noreply.github.com>
2022-02-10 21:49:09 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function inputPaste() {
+    /* GET INPUT */
+    const name = document.getElementById('name');
+    const age = document.getElementById('age');
+    const skill = document.getElementById('skills');
+
+    /*GET OUTPUT */
+    const nameOutput = document.getElementById('name-output');
+    const ageOutput = document.getElementById('age-output');
+    const skillOutput = document.getElementById('skill-output');
+
+    /* overwriting  */
+    if (name.value != "" && age.value != "" && skills.value != ""){
+        nameOutput.innerHTML = name.value;
+        ageOutput.innerHTML = age.value;
+        skillOutput.innerHTML = skill.value;
+
+        const result = document.getElementById('result');
+        const resultArea = document.getElementById('result-area');
+        resultAreaClone = resultArea.cloneNode(true);
+        result.appendChild(resultAreaClone);
+        resultArea.style.display = 'none';
+        resultAreaClone.style.display = 'block';
+    }
+    else {
+        alert('Por favor, preencha TODOS os dados.');
+    }
+    
+}
